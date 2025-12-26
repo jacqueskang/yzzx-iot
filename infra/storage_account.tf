b5514cb87df84bca14bfa75d8c260297a42bd07d
Jacques Kang <jkang.perso@gmail.com>
2025-12-26 18:47:02 +0100

    fix: disable shared access key for strorage account

--- infra/storage_account.tf
+++ infra/storage_account.tf
@@ -2,9 +2,11 @@ resource "azurerm_storage_account" "main" {
   name                            = "func${local.suffix_clean}${random_string.sa_suffix.result}"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
+  account_kind                    = "StorageV2"
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
   tags                            = var.tags
+  shared_access_key_enabled       = false
 }
