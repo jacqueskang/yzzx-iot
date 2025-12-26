72b712016103f5c349d05b2a790e79e75f3a2871
Jacques Kang <jkang.perso@gmail.com>
2025-12-26 16:39:36 +0100

    feat: switch to managed identity for accessing storage account

--- infra/role_assignment.tf
+++ infra/role_assignment.tf
@@ -1,3 +1,8 @@
+resource "azurerm_role_assignment" "func_storage_blob_contributor" {
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
 resource "azurerm_role_assignment" "func_azure_digital_twins_data_owner" {
   scope                = azurerm_digital_twins_instance.main.id
   role_definition_name = "Azure Digital Twins Data Owner"
