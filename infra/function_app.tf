ddc7b1b8a48f701a876ad8d87d402a6bf89c0e30
Jacques Kang <jkang.perso@gmail.com>
2025-12-26 16:39:36 +0100

    feat: switch to managed identity for accessing storage account

--- infra/function_app.tf
+++ infra/function_app.tf
@@ -4,7 +4,7 @@ resource "azurerm_linux_function_app" "main" {
   resource_group_name         = azurerm_resource_group.main.name
   service_plan_id             = azurerm_service_plan.main.id
   storage_account_name        = azurerm_storage_account.main.name
-  storage_account_access_key  = azurerm_storage_account.main.primary_access_key
+  storage_uses_managed_identity = true
   functions_extension_version = "~4"
 
   identity {
@@ -20,10 +20,9 @@ resource "azurerm_linux_function_app" "main" {
   }
 
   app_settings = {
-    FUNCTIONS_WORKER_RUNTIME           = "node"
-    WEBSITE_RUN_FROM_PACKAGE           = "1"
-    ADT_URL                            = "https://${azurerm_digital_twins_instance.main.host_name}"
-    AzureWebJobsStorage__accountName   = azurerm_storage_account.main.name
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+    ADT_URL                  = "https://${azurerm_digital_twins_instance.main.host_name}"
   }
 
   tags = var.tags
