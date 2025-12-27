5b00988698a60a590b3ac3aec7e5267498f3beef
Jacques Kang <jkang.perso@gmail.com>
2025-12-27 17:12:33 +0100

    fix: terraform format

--- infra/service_plan.tf
+++ infra/service_plan.tf
@@ -1,8 +1,8 @@
 resource "azurerm_service_plan" "main" {
-    name                = "asp-${var.suffix}"
-    location            = var.location
-    resource_group_name = azurerm_resource_group.main.name
-    os_type             = "Linux"
-    sku_name            = "FC1"
-    tags                = var.tags
+  name                = "asp-${var.suffix}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "FC1"
+  tags                = var.tags
 }
