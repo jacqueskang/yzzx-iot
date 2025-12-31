1f51098a320bc202077c606a66ef2a1fe1724046
Jacques Kang <jkang.perso@gmail.com>
2025-12-31 22:30:23 +0100

    refactor(webapp): move API dependencies to api/package.json, clean up frontend package.json, and standardize Azure Functions structure

---
+++ webapp/api/getDeviceStatus/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = default_1;
+async function default_1() {
+    return {
+        body: [
+            { id: "light1", x: 100, y: 200, on: true },
+            { id: "sensor1", x: 400, y: 300, on: false },
+        ],
+    };
+}
