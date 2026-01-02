0fd24460550efd5c60de8d5b65ba0a2b2c3afd82
Jacques Kang <jkang.perso@gmail.com>
2026-01-02 20:33:43 +0100

    style(webapp): fix line endings in FloorPlanMap.vue

--- webapp/src/components/FloorPlanMap.vue
+++ webapp/src/components/FloorPlanMap.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="floor-plan">
-    <svg class="floor-plan__svg" width="100%" height="100%" viewBox="0 0 800 600" preserveAspectRatio="xMidYMid meet">
+    <svg
+      class="floor-plan__svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 800 600"
+      preserveAspectRatio="xMidYMid meet"
+    >
       <!-- Example: Floor plan background -->
       <image href="/floorplan.svg" x="0" y="0" width="800" height="600" />
       <!-- Example: Device icon overlay -->
-      <circle v-for="device in devices" :key="device.id" :cx="device.x" :cy="device.y" r="16"
-        :fill="device.on ? 'green' : 'red'" />
+      <circle
+        v-for="device in devices"
+        :key="device.id"
+        :cx="device.x"
+        :cy="device.y"
+        r="16"
+        :fill="device.on ? 'green' : 'red'"
+      />
     </svg>
   </div>
 </template>
