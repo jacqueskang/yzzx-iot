8f53bad87795c989cd8d6322a88d2026f0d7c7f3
Jacques Kang <jkang.perso@gmail.com>
2026-01-03 10:34:41 +0100

    refactor(infra): remove SWA managed identity and configure function app CORS

--- infra/role_assignment.tf
+++ infra/role_assignment.tf
@@ -14,9 +14,3 @@ resource "azurerm_role_assignment" "func_eventhub_receiver" {
   role_definition_name = "Azure Event Hubs Data Receiver"
   principal_id         = azurerm_function_app_flex_consumption.main.identity[0].principal_id
 }
-
-resource "azurerm_role_assignment" "swa_azure_digital_twins_data_owner" {
-  scope                = azurerm_digital_twins_instance.main.id
-  role_definition_name = "Azure Digital Twins Data Owner"
-  principal_id         = azurerm_static_web_app.main.identity[0].principal_id
-}
