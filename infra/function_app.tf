760748de0ceb6d9408205ec7b9da279f0efed17a
Jacques Kang <jkang.perso@gmail.com>
2026-01-03 10:34:41 +0100

    refactor(infra): remove SWA managed identity and configure function app CORS

--- infra/function_app.tf
+++ infra/function_app.tf
@@ -1,19 +1,17 @@
 resource "azurerm_function_app_flex_consumption" "main" {
-  name                = "func-${var.suffix}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  service_plan_id     = azurerm_service_plan.main.id
-
+  name                        = "func-${var.suffix}"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  service_plan_id             = azurerm_service_plan.main.id
   storage_container_type      = "blobContainer"
   storage_container_endpoint  = "${azurerm_storage_account.main.primary_blob_endpoint}${azurerm_storage_container.function.name}"
   storage_authentication_type = "StorageAccountConnectionString"
   storage_access_key          = azurerm_storage_account.main.primary_access_key
-
-  runtime_name    = "node"
-  runtime_version = "20"
-
-  maximum_instance_count = 40
-  instance_memory_in_mb  = 512
+  runtime_name                = "node"
+  runtime_version             = "20"
+  maximum_instance_count      = 40
+  instance_memory_in_mb       = 512
+  tags                        = var.tags
 
   app_settings = {
     ADT_SERVICE_URL         = "https://${azurerm_digital_twins_instance.main.host_name}"
@@ -29,17 +27,22 @@ resource "azurerm_function_app_flex_consumption" "main" {
   }
 
   site_config {
-    minimum_tls_version = "1.2"
-
+    minimum_tls_version                    = "1.2"
     application_insights_key               = azurerm_application_insights.main.instrumentation_key
     application_insights_connection_string = azurerm_application_insights.main.connection_string
+
+    cors {
+      allowed_origins = [
+        "https://${azurerm_static_web_app.main.default_host_name}",
+        "http://localhost:5173",
+      ]
+      support_credentials = false
+    }
   }
 
   identity {
     type = "SystemAssigned"
   }
-
-  tags = var.tags
 
   lifecycle {
     ignore_changes = [
